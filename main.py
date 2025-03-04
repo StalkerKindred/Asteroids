fe3e7501156e1ca10659169c3e73e53d590b0caf
StalkerKindred <MMMeriaddd@gmail.com>
2025-03-04 17:48:19 +0100

    Updated Loop logic, added arrows support

--- main.py
+++ main.py
@@ -13,14 +13,19 @@ def main():
     dt = 0
     player = Player(x,y)
     while True:
+        #dt
+        dt = clock.tick(60) / 1000
+        #Exiting
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #Update
+        player.update(dt)
+        #Drawing
         pygame.Surface.fill(screen, (0,0,0))
         player.draw(screen)
-        player.update(dt)
+        #Flip
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
